c4b49e56c53f5e4d6c6aae342b8441d3efdc1cd3
Anton <geraskin.antony@yandex.ru>
2021-10-26 22:08:45 +0700

    -кнопки ответа блокируются для вопроса на который уже был дан ответ -ведётся счёт правильных ответов пользователя и выводится на экран

--- app/src/main/java/com/bignerdranch/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/geoquiz/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton:Button
     private lateinit var falseButton:Button
+    private lateinit var resetButton:Button
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView:TextView
@@ -26,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     )
 
     private var currentIndex = 0
+    private var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,14 +37,17 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
+        resetButton = findViewById(R.id.reset_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
             checkAnswer(true)
+            getUserScore()
         }
 
         falseButton.setOnClickListener {
             checkAnswer(false)
+            getUserScore()
         }
 
         nextButton.setOnClickListener {
@@ -58,8 +63,11 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
-
-
+        resetButton.setOnClickListener {
+            questionBank.forEach { it.userAnswer = null }
+            setButtonState()
+            score = 0
+        }
 
         questionTextView.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
@@ -69,14 +77,22 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    private fun setUserAnswer(userAnswer: Boolean){
+        if (questionBank[currentIndex].answer == userAnswer)
+            score++
+        questionBank[currentIndex].userAnswer = userAnswer
+        setButtonState()
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        setButtonState()
     }
 
     private fun checkAnswer(userAnswer:Boolean){
         val correctAnswer = questionBank[currentIndex].answer
-
+        setUserAnswer(userAnswer)
         val messageResId = if (userAnswer == correctAnswer)
             R.string.correct_toast
         else R.string.incorrect_toast
@@ -87,4 +103,25 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT)
             .show()
         }
+
+    private fun setButtonState(){
+        if (questionBank[currentIndex].userAnswer != null){
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
+        }
+        else
+        {
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
+        }
+    }
+
+    private fun getUserScore(){
+        if (questionBank.all{it.userAnswer != null})
+                Toast.makeText(
+                    this,
+                    "Your score: $score/6",
+                    Toast.LENGTH_LONG)
+                    .show()
+    }
 }
--- app/src/main/java/com/bignerdranch/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/geoquiz/Question.kt
@@ -3,5 +3,5 @@ package com.bignerdranch.geoquiz
 import androidx.annotation.StringRes
 
 data class Question(@StringRes val textResId: Int, val answer: Boolean) {
-
+    var userAnswer:Boolean? = null
 }
